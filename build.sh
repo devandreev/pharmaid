f3727e2905fcbf19d30c996162057bd961889cce
defined <defined@MacBook-Pro-donotyou.local>
2026-09-02 00:25:55 +0300

    init build

--- build.sh
+++ build.sh
@@ -7,27 +7,27 @@ timestamp() {
 }
 
 cp -r ./public/images ./public/dist/images
-cp ./public/favicon.ico ./public/dist/favicon.ico
+#cp -r ./public/videos ./public/dist/videos
+cp -r ./public/images_avif ./public/dist/images_avif
+#cp ./public/site.webmanifest ./public/dist/site.webmanifest
+#cp ./public/favicon.ico ./public/dist/favicon.ico
+#cp ./public/favicon.svg ./public/dist/favicon.svg
+#cp ./public/favicon-96x96.png ./public/dist/favicon-96x96.png
+#cp ./public/apple-touch-icon.png ./public/dist/apple-touch-icon.png
+#cp ./public/web-app-manifest-192x192.png ./public/dist/web-app-manifest-192x192.png
+#cp ./public/web-app-manifest-512x512.png ./public/dist/web-app-manifest-512x512.png
 
 cd ./public/dist
 
-grep -rli '/images/' * | xargs -i@ sed -i 's/\/images\//\.\/images\//g' @
-grep -rli '.css"' * | xargs -i@ sed -i "s/\.css\"/\.css?v=$(timestamp)\"/g" @
-grep -rli '.js"' * | xargs -i@ sed -i "s/\.js\"/\.js?v=$(timestamp)\"/g" @
+grep -rli '/images/' * | xargs -I@ sed -i '' 's/\/images\//\.\/images\//g' @
+#grep -rli '/videos/' * | xargs -I@ sed -i '' 's/\/videos\//\.\/videos\//g' @
+grep -rli '/images_avif/' * | xargs -I@ sed -i '' 's/\/images_avif\//\.\/images_avif\//g' @
+grep -rli '.css"' * | xargs -I@ sed -i '' "s/\.css\"/\.css?v=${timestamp}\"/g" @
+grep -rli '.js"' * | xargs -I@ sed -i '' "s/\.js\"/\.js?v=${timestamp}\"/g" @
 
-cd assets/js
-
-# Удаление переносов строк в шаблонных литералах
-cat base.bundle.js | tr -d '\n' > temp.bundle.js
-mv temp.bundle.js base.bundle.js
-
-rm styles.bundle.js
-
-cd ../../
-
-# git init
-# git add --all
-# git commit -m "build"
-# git branch -M dist
-# git remote add origin git@github.com:username/repo.git
-# git push -u origin dist --force
+ git init
+ git add --all
+ git commit -m "build"
+ git branch -M dist
+ git remote add origin git@github.com:devandreev/pharmaid.git
+ git push -u origin dist --force
